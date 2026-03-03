44dc7f9a508612b8dc7d5f0bc21fa709886dbf38
rohit <rohitkumarturangi17@gmail.com>
2026-03-03 13:31:09 +0530

    varialbes

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,14 +1,19 @@
 #! bin/bash
 
-number=20
+number1=20
+number2=20
 
-if ($number -lt 10)
+number3=$sum
+
+
+if ($number1 -eq $number2)
 then 
 
-echo "here $number is less then 10 "
+sum=$(($number+$number))
+echo "here is $number3 "
 
 else 
 
-echo "$number is not less then 10"
+echo "$number3 is not equal to $number1"
 
 fi
