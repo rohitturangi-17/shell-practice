ddad06789e5d12c93459c529d010463db70dc727
rohit <rohitkumarturangi17@gmail.com>
2026-04-07 12:45:35 +0530

    usage alert

--- usagedisk.sh
+++ usagedisk.sh
@@ -8,24 +8,26 @@ N="\e[0m"
 DISK_THRESHOLD=20
 MSG=""
 
-# Get private IP (works only in cloud like AWS)
 IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
-# Get disk usage excluding header
-df -hT | awk 'NR>1 {print}' | while IFS= read -r line
+# Use process substitution instead of pipe (fix subshell issue)
+while IFS= read -r line
 do
     USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo "$line" | awk '{print $7}')
     FILESYSTEM=$(echo "$line" | awk '{print $1}')
-    EMAIL=$(echo -e "$MSG" | mail -s "Disk Alert - $IP" rohitkumarturangi17@gmail.com)
 
     if [ "$USAGE" -ge "$DISK_THRESHOLD" ]
     then 
         MSG+="$R Disk Usage exceeded on $FILESYSTEM ($PARTITION): $USAGE% $N\n"
     fi
-done
+done < <(df -hT | awk 'NR>1')
 
 # Print message
 echo -e "$MSG"
-echo -e "$EMAIL"
 
+# Send email ONLY if alert exists
+if [ -n "$MSG" ]
+then
+    echo -e "$MSG" | mail -s "Disk Alert - $IP" rohitkumarturangi17@gmail.com
+fi
