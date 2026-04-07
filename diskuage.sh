0835c24fc337fb30dfd38a94263e87ca295e22d8
rohit <rohitkumarturangi17@gmail.com>
2026-04-07 12:17:17 +0530

    diskusage

--- diskuage.sh
+++ diskuage.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -v Filesystem)
-TMPFS=$(df -hT | grep -v tmpfs)
-DISK_THRESHOLD=2 # in project it will be 75
+DISK_USAGE=$(df -hT | grep -vE 'Filesystem|tmpfs')
+TMPFS_USAGE=$(df -hT | grep tmpfs)
+
+DISK_THRESHOLD=2   # ideally 75 in real case
 MSG=""
-IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
-while IFS= read line
+IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+# Check normal disks
+while IFS= read -r line
 do
-    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $7F}')
-    TMPFS_USAGE=$(echo $line | awk '{print $2F,$3F,$4F}' | cut -d "%" -f1)
-    TMPFS_PARTITION=$(echo $line | awk '{print $7F}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
-    then
-        #MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
-        MSG+="High Disk Usage on $PARTITION: $USAGE % \n"
-        MSG+="High Disk Usage on $TMPFS_PARTITION: $TMPFS_USAGE % \n"
-        
-    fi
-done <<< $DISK_USAGE $TMPFS
+    USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo "$line" | awk '{print $7}')
 
-echo -e $MSG
+    if [ "$USAGE" -ge "$DISK_THRESHOLD" ]
+    then
+        MSG+="High Disk Usage on $PARTITION: $USAGE% \n"
+    fi
+done <<< "$DISK_USAGE"
+
+# Check tmpfs separately (optional)
+while IFS= read -r line
+do
+    USAGE=$(echo "$line" | awk '{print $6}' | cut -d "%" -f1)
+    PARTITION=$(echo "$line" | awk '{print $7}')
+
+    if [ "$USAGE" -ge "$DISK_THRESHOLD" ]
+    then
+        MSG+="TMPFS Usage on $PARTITION: $USAGE% \n"
+    fi
+done <<< "$TMPFS_USAGE"
+
+echo -e "$MSG"
