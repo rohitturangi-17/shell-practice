cc097f536d0d58612f7363c7e8eb388d4fc9896a
rohit <rohitkumarturangi17@gmail.com>
2026-04-07 12:10:28 +0530

    diskusage

--- diskuage.sh
+++ diskuage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+TMPFS=$(df -hT | awk '$1=="tmpfs" {print $4, $5, $6}')
 DISK_THRESHOLD=2 # in project it will be 75
 MSG=""
 IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
@@ -13,6 +14,7 @@ do
     then
         #MSG+="High Disk Usage on $PARTITION: $USAGE % <br>" #<br> represents HTML new
         MSG+="High Disk Usage on $PARTITION: $USAGE % \n"
+        echo "Listed tmps : $TMPFS"
     fi
 done <<< $DISK_USAGE
 
